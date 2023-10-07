50a146c6b97edc631d6a1e2c5f7fab7f43a8d74d
Aayush <aayushj2001@gmail.com>
2023-10-07 14:24:32 +0545

    feat:  new changes in project with one dependency added

--- Deepblue-Voyage/src/App.jsx
+++ Deepblue-Voyage/src/App.jsx
@@ -1,10 +1,15 @@
-import { QueryClientProvider, QueryClient } from "react-query";
+// import { QueryClientProvider, QueryClient } from "react-query";
+import Home from "./components/main/Home";
 
 function App() {
   return (
-    <QueryClientProvider client={QueryClient}>
-      <h1>Hello world</h1>
-    </QueryClientProvider>
+    // <QueryClientProvider client={QueryClient}>
+    //   <h1>Hello world</h1>
+    // </QueryClientProvider>
+    // eslint-disable-next-line react/jsx-no-comment-textnodes
+    <>
+      <Home />
+    </>
   );
 }
 
