32e7032598c8487d579833589072f2237649c51f
Hunter-420 <khanalnischal2075@gmail.com>
2023-10-08 00:09:01 +0545

    minor bug fixed

--- Deepblue-Voyage/src/components/main/Home.jsx
+++ Deepblue-Voyage/src/components/main/Home.jsx
@@ -1,10 +1,18 @@
 import videoBg from "../../assets/videoBg.mp4";
 import "../../index.css";
+import Quiz from "./Quiz";
 const Home = () => {
   return (
+    <>
     <div className="main">
       <video src={videoBg} autoPlay loop muted/>
     </div>
+    <div>
+      <h1 className="main-title">Deepblue Voyage</h1>
+      <h2 className="main-subtitle">A quiz app about the ocean</h2>
+      <Quiz />
+    </div>
+    </>
   );
 };
 
---
+++ Deepblue-Voyage/src/components/main/Quiz.jsx
@@ -0,0 +1,10 @@
+function Quiz(){
+
+    return(
+        <div>
+            <h1>Quiz</h1>
+        </div>
+    )
+}
+
+export default Quiz;
