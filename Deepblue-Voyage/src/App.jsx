e2a008574925c3c1064e533e3873fcfa7967d32f
Aayush <aayushj2001@gmail.com>
2023-10-08 05:29:04 +0545

    feat: new section changes

--- Deepblue-Voyage/src/App.jsx
+++ Deepblue-Voyage/src/App.jsx
@@ -5,35 +5,90 @@ import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import "./index.css";
 import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
 import SecondSection from "../src/components/SecondSection/SecondSection";
-import ThirdSection from "./components/ThirdSection/ThirdSection";
+import ThirdSection from "../src/components/ThirdSection/ThirdSection";
+import { Typography, createMuiTheme } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
+import AquaImg from "../src/assets/images/jpg/Aqua.jpg";
 function App() {
   const ref = useRef();
+  const theme = createMuiTheme({
+    typography: { fontFamily: ["Orbitron", "sans-serif"].join(",") },
+  });
   return (
     // <QueryClientProvider client={QueryClient}>
     //   <h1>Hello world</h1>
     // </QueryClientProvider>
     // eslint-disable-next-line react/jsx-no-comment-textnodes
     <>
-      <div>
-        <Parallax pages={5} ref={ref}>
-          <ParallaxLayer speed={2.5}>
-            <Home />
-          </ParallaxLayer>
-          <ParallaxLayer offset={1}>
-            <SecondSection />
-          </ParallaxLayer>
-          <ParallaxLayer offset={1} speed={1}>
-            <ThirdSection />
-          </ParallaxLayer>
-          <ParallaxLayer offset={3} speed={1} className="button">
-            <ArrowCircleUpIcon
-              fontSize="large"
-              onClick={() => ref.current.scrollTo(0)}
-            ></ArrowCircleUpIcon>
-            <p>Scroll to top</p>
-          </ParallaxLayer>
-        </Parallax>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div>
+          <Parallax pages={4} ref={ref}>
+            <ParallaxLayer offset={0} speed={1}>
+              <Home />
+            </ParallaxLayer>
+            <ParallaxLayer offset={1.1} sticky={{ start: 0.9, end: 0.92 }}>
+              <Typography
+                align="center"
+                justifyContent="center"
+                noWrap
+                variant="h2"
+              >
+                Lets Begin our Journey
+              </Typography>
+            </ParallaxLayer>
+            <ParallaxLayer
+              offset={1}
+              sticky={{ start: 1, end: 1.2 }}
+              style={{
+                backgroundImage: `url[${AquaImg}]`,
+                backgroundSize: "cover",
+              }}
+              speed={1}
+            >
+              <SecondSection />
+            </ParallaxLayer>
+
+            <ParallaxLayer offset={1.7} sticky={{ start: 1.9, end: 1.92 }}>
+              <Typography
+                align="center"
+                justifyContent="center"
+                noWrap
+                variant="h2"
+              >
+                Live location of Phytoplankton
+              </Typography>
+            </ParallaxLayer>
+
+            <ParallaxLayer sticky={{ start: 2, end: 2.2 }} offset={2} speed={1}>
+              <ThirdSection />
+            </ParallaxLayer>
+
+            <ParallaxLayer offset={3.2} sticky={{ start: 3.2, end: 3.2 }}>
+              <Typography
+                align="center"
+                justifyContent="center"
+                noWrap
+                variant="h2"
+              >
+                About Us
+              </Typography>
+            </ParallaxLayer>
+
+            <ParallaxLayer
+              offset={3.99}
+              speed={2.5}
+              className="button"
+              sticky={{ start: 3.99, end: 3.99 }}
+            >
+              <ArrowCircleUpIcon
+                fontSize="large"
+                onClick={() => ref.current.scrollTo(0)}
+              ></ArrowCircleUpIcon>
+              <p>Scroll to top</p>
+            </ParallaxLayer>
+          </Parallax>
+        </div>
+      </ThemeProvider>
     </>
   );
 }
--- Deepblue-Voyage/src/components/SecondSection/SecondSection.jsx
+++ Deepblue-Voyage/src/components/SecondSection/SecondSection.jsx
@@ -4,6 +4,7 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Unstable_Grid2";
 // import NASA1 from "../../assets/images/jpg/nasa1.jpg";
 import EarthBg from "../../assets/EarthBg.mp4";
+import AquaImg from "../../assets/images/jpg/Aqua.jpg";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -15,13 +16,18 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function SecondSection() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box
+      sx={{ flexGrow: 1 }}
+      style={{
+        backgroundImage: `url(${AquaImg})`,
+      }}
+    >
       <Grid container spacing={2} columns={16}>
         <Grid xs={8}>
           <Item>Lorem Lipsum</Item>
         </Grid>
         <Grid xs={8}>
-          <Item>
+          <Item style={{ padding: "0px", margin: "0px", border: "opx" }}>
             <video src={EarthBg} alt="EarthBg" autoPlay loop />
           </Item>
         </Grid>
